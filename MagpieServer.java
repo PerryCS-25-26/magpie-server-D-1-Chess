e6a6eda10582b5c0c63fea46280b46e090715ee8
Brody Sweetser <brodysweetser@gmail.com>
2025-11-25 10:31:22 -0700

    Copy for magpieserver +

---
+++ MagpieServer.java
@@ -0,0 +1,13 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+public class MagpieServer implements HttpHandler 
+{
+    public void handle(HttpExchange exchange)
+    {
+        
+    }
+}
